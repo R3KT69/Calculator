cffea9c928ac4d565bbbfc07297ebe7406ffd1db
_r3kt69 <74340727+R3KT69@users.noreply.github.com>
2021-10-23 11:22:38 +0600

    Source code

---
+++ source code.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    float num1;
+    float num2;
+    char op;
+    char userInput;
+    bool programClose = false;
+
+    while (programClose != true)
+    {
+        printf("(N) to exit the program. Enter to continue\n");
+        scanf("%c", &userInput);
+
+        if (userInput == 'N')
+        {
+            break;
+        } else if (userInput != 'N')
+        {}
+
+        printf("\nEnter Numbers: ");
+        scanf("%f %c %f", &num1, &op, &num2);
+
+        if  (op == '+')
+        {
+            printf("Answer: %f \n\n", num1 + num2);
+        } else if (op == '-')
+        {
+            printf("Answer: %f \n\n", num1 - num2);
+        } else if (op == '/')
+        {
+            printf("Answer: %f \n\n", num1 / num2);
+        } else if (op == '*')
+        {
+            printf("Answer: %f \n\n", num1 * num2);
+        } else
+        {
+            printf("\nInvalid Command.\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
